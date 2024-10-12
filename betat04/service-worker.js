feec87deb7d7da21b7582ae626e80c5b688efacc
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2024-10-12 06:46:29 -0300

    Parte final
    Con PWA

---
+++ betat04/service-worker.js
@@ -0,0 +1,24 @@
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches.open('my-cache').then((cache) => {
+        return cache.addAll([
+          '/',
+          '/index.html',
+          '/sketch.js', // Cambia este nombre si es diferente
+          '/manifest.json',
+          '/icon-192.png',
+          '/icon-512.png',
+          // Agrega aquí más archivos como imágenes o fuentes
+        ]);
+      })
+    );
+  });
+  
+  self.addEventListener('fetch', (event) => {
+    event.respondWith(
+      caches.match(event.request).then((response) => {
+        return response || fetch(event.request);
+      })
+    );
+  });
+  
